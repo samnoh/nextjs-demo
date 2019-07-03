f3134f75ef6136b6bd82e587203ecd49254a0c15
Sam Noh <samnoh21@outlook.com>
2019-07-03 15:24:56 +1200

    add - webpack settings

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
-module.exports = {
-    distDir: 'build'
-};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = withBundleAnalyzer({
+    distDir: 'build',
+    webpack(config) {
+        const prod = process.env.NODE_ENV === 'production';
+        const plugins = [
+            ...config.plugins,
+            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/)
+        ];
+        if (prod) {
+            plugins.push(new CompressionPlugin()); // main.js.gz
+        }
+
+        return {
+            ...config,
+            mode: prod ? 'production' : 'development',
+            devtool: prod ? 'hidden-source-map' : 'eval',
+            plugins
+        };
+    }
+});
