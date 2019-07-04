bfe472da1f23f8598dbe9a4580f55e64192b74a7
Sam Noh <samnoh21@outlook.com>
2019-07-04 13:11:13 +1200

    mod - cookie

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.prepare().then(() => {
             secret: process.env.COOKIE_SECRET,
             cookie: {
                 httpOnly: true,
-                secure: false
+                secure: prod
             }
         })
     );
