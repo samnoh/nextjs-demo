9acd1d5c6369eb8f46989bfe3a89f2ebaa3f1d6d
Sam Noh <samnoh21@outlook.com>
2019-07-04 13:09:05 +1200

    add - axios.defaults.baseURL

--- modules/index.js
+++ modules/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
+import axios from 'axios';
 
 import posts, { postsSaga } from './posts';
 import loading from './loading';
+
+// const backEndUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000';
+
+axios.defaults.baseURL = 'http://localhost:5000';
 
 const rootReducer = combineReducers({
     posts,
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const next = require('next');
 const path = require('path');
 const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const dev = process.env.NODE_ENV !== 'production';
 const prod = process.env.NODE_ENV === 'production';
@@ -19,6 +23,20 @@ app.prepare().then(() => {
     server.use(helmet());
     server.use(compression());
     server.use('/', express.static(path.join(__dirname, 'public')));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+    server.use(cookieParser(process.env.COOKIE_SECRET));
+    server.use(
+        expressSession({
+            resave: false,
+            saveUninitialized: false,
+            secret: process.env.COOKIE_SECRET,
+            cookie: {
+                httpOnly: true,
+                secure: false
+            }
+        })
+    );
 
     server.get('*', (req, res) => {
         return handle(req, res);
